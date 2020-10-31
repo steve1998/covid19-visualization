3551c0d0827c5110b22336fcb36425aa90319eab
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-10-31 15:56:08 -0700

    Create Base Layout

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchData } from '../../middleware/api';
+import './Dashboard.scss';
 
 export default function Dashboard() {
     const [data, setData] = useState<Array<any>>([]);
@@ -13,8 +14,17 @@ export default function Dashboard() {
     useEffect(() => {}, [setData]);
 
     return(
-        <div>
-            <h1>COVID-19 Dashboard</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <span className="title">COVID-19 Statistics (US)</span>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <span>Made by Nicholas Steven Darmawan</span>
+                </div>
+            </div>
         </div>
     )
 }
