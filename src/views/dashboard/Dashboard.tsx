2f4f0348299f1165656d54e41fb4991df1afccef
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-10-31 16:02:41 -0700

    Add Descriptions

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -18,9 +18,12 @@ export default function Dashboard() {
             <div className="row">
                 <div className="col">
                     <span className="title">COVID-19 Statistics (US)</span>
+                    <div className="w-100"></div>
+                    <span>The idea for this COVID-19 statistics tracker is to provide a cleaner and simpler take on some of the important data on COVID-19. This dashboard is made using D3.js to visualize the data and the dataset is provided by Google.</span>
+                    <div className="w-100"></div>
                 </div>
             </div>
-            <div className="row">
+            <div className="row pt-4">
                 <div className="col">
                     <span>Made by Nicholas Steven Darmawan</span>
                 </div>
