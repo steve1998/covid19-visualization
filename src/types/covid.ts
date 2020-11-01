8a972ae52a6f68519fd29d49d6ca4f837eafb657
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-10-31 17:07:44 -0700

    Create covid.ts

---
+++ src/types/covid.ts
@@ -0,0 +1,13 @@
+export interface CovidObject {
+    "date": string;
+    "new_confirmed": number,
+    "new_deceased": number,
+    "new_recovered": number,
+    "new_tested": number,
+    "total_confirmed": number,
+    "total_deceased": number,
+    "total_recovered": number,
+    "total_tested": number,
+    "new_hospitalized": number,
+    "total_hospitalized": number
+}
