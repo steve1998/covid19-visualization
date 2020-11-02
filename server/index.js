db65c2c16e4258d1a80b4d816b872209d98227ac
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-11-02 14:58:24 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,5 +13,5 @@ app.use((req, res, next) => {
 // start express server on port 5000
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log("server started on port 5000");
+    console.log("Server started on $PORT");
 });
