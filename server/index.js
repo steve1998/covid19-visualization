95dd044347dee8f4cbc72ce0bb4c29193115c595
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-11-03 10:44:02 -0800

    Add GraphQL

--- server/index.js
+++ server/index.js
@@ -13,5 +13,5 @@ app.use((req, res, next) => {
 // start express server on port 5000
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log("Server started on $PORT");
+    console.log(`Server started on ${PORT}`);
 });
