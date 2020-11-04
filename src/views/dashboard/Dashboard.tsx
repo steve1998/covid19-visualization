825f92ff2ba2933d6100081fa31e158a4f340ac7
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-11-04 14:01:55 -0800

    Update Dashboard.tsx

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -85,10 +85,6 @@ export default function Dashboard() {
                 <Row className="pt-4">
                     <Col>
                         <div className="heading-section" onClick={toggleTotal}>
-                            <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-link-45deg mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
-                                <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/>
-                            </svg>
                             <span className="heading">Total Cases and Deaths to Date</span>
                         </div>
                         <Collapse isOpen={isOpenTotal}>
@@ -101,10 +97,6 @@ export default function Dashboard() {
                 <Row className="pt-4">
                     <Col>
                         <div className="heading-section" onClick={toggleNew}>
-                            <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-link-45deg mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
-                                <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/>
-                            </svg>
                             <span className="heading">New Cases and Deaths to Date</span>
                         </div>
                         <Collapse isOpen={isOpenNew}>
@@ -117,10 +109,6 @@ export default function Dashboard() {
                 <Row className="pt-4">
                     <Col>
                         <div className="heading-section" onClick={toggleState}>
-                            <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-link-45deg mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
-                                <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/>
-                            </svg>
                             <span className="heading">Total Cases and Deaths per State</span>
                         </div>
                         <Collapse isOpen={isOpenState}>
