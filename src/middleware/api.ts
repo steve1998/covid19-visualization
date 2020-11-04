f966a5027332400f889b337c1ac0d68007b7c884
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-11-03 20:05:53 -0800

    Clean Up Code

--- src/middleware/api.ts
+++ src/middleware/api.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
 export async function fetchData(key: string) {
-    // let baseURL: string = process.env.PORT + '/fetchData' || 'http://localhost:5000/fetchData';
     const response: AxiosResponse = await axios.get('/fetchData', {
         params: {
             key: 'US'
@@ -11,26 +10,3 @@ export async function fetchData(key: string) {
     const data = response.data;
     return data;
 }
-
-// function processData(data: any) {
-//     const records: Array<any> = data.data;
-//     let processedData: Array<CovidObject> = [];
-
-//     records.forEach(record => {
-//         if (record[legend.date] !== null && record[legend.total_confirmed] !== null && record[legend.total_deceased] !== null && record[legend.new_confirmed] !== null && record[legend.new_deceased] !== null) {
-//             const date: number = Date.parse(record[legend.date]);
-
-//             let object: any = {
-//                 "date": date,
-//                 "new_confirmed": record[legend.new_confirmed],
-//                 "new_deceased": record[legend.new_deceased],
-//                 "total_confirmed": record[legend.total_confirmed],
-//                 "total_deceased": record[legend.total_deceased],
-//             };
-
-//             processedData.push(object);
-//         }
-//     });
-        
-//     return processedData;
-// }
